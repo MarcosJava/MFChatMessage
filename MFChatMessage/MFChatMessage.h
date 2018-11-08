58db8da3835a39beae7804151c4a146c08ffde58
Marcos Felipe Souza <mfelipesp@gmail.com>
2018-11-07 23:17:10 -0200

    Initial Commit

---
+++ MFChatMessage/MFChatMessage.h
@@ -0,0 +1,19 @@
+//
+//  MFChatMessage.h
+//  MFChatMessage
+//
+//  Created by Marcos Felipe Souza on 07/11/18.
+//  Copyright © 2018 Marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MFChatMessage.
+FOUNDATION_EXPORT double MFChatMessageVersionNumber;
+
+//! Project version string for MFChatMessage.
+FOUNDATION_EXPORT const unsigned char MFChatMessageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MFChatMessage/PublicHeader.h>
+
+
